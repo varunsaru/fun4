65f521d1ad94a9bde50451c479e345a2d7844d5c
varunsaru <ravindransarangan2013@gmail.com>
2018-10-15 12:30:24 +0530

    heroku

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ var app = express();
 app.use(express.static(path.join(__dirname, "../app/dist")));
 app.use(bodyParser.json())
 app.use("/api", schoolController);
-app.get('/index.html', (req, res)=>{
+app.get('/', (req, res)=>{
     res.render('index', { user : req.user});
   })
 app.listen(7777, function () {
