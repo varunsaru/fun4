6fd9afc2b04e0083546b6ca75a7f76a4c6743174
varunsaru <ravindransarangan2013@gmail.com>
2018-10-15 12:28:48 +0530

    heroku

--- server/server.js
+++ server/server.js
@@ -12,7 +12,9 @@ var app = express();
 app.use(express.static(path.join(__dirname, "../app/dist")));
 app.use(bodyParser.json())
 app.use("/api", schoolController);
-
+app.get('/index.html', (req, res)=>{
+    res.render('index', { user : req.user});
+  })
 app.listen(7777, function () {
     console.log("Started listening on port", 7777);
 });
