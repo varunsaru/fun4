6f6e0ffa21ade354e6af20b8bb4271ce65c07852
varunsaru <ravindransarangan2013@gmail.com>
2018-10-15 12:05:55 +0530

    heroku

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var path = require("path");
-
+var port = process.env.PORT || 8000;
 //controllers
 var schoolController = require("./controllers/schoolController");
 
@@ -12,8 +12,11 @@ app.use(express.static(path.join(__dirname, "../app/dist")));
 app.use(bodyParser.json())
 app.use("/api", schoolController);
 
-app.listen(7777, function () {
-    console.log("Started listening on port", 7777);
+// app.listen(7777, function () {
+//     console.log("Started listening on port", 7777);
+// });
+server.listen(port, function() {
+    console.log("App is running on port " + port);
 });
 
 mongoose.connect("mongodb://Sarangan:saru0528-@ds133113.mlab.com:33113/schoolfinder");
