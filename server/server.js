36070c28b3c4ccf4dc1a16792365e3004256af19
varunsaru <ravindransarangan2013@gmail.com>
2018-10-15 14:29:26 +0530

    dats

--- server/server.js
+++ server/server.js
@@ -9,11 +9,11 @@ var schoolController = require("./controllers/schoolController");
 
 //Express request pipeline
 var app = express();
-app.use(express.static(path.join(__dirname, "../app/dist")));
+app.use(express.static(path.join(__dirname, "./app/dist")));
 app.use(bodyParser.json())
 app.use("/api", schoolController);
 
-app.listen(7777, function () {
+app.listen(port, function () {
     console.log("Started listening on port", 7777);
 });
 // server.listen(port, () => {
